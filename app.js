9154f90cc469727cff445d3eef201c8929ab600c
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2020-03-11 17:02:48 -0400

    adds key sequence to array. if array matches code word, then alerts user

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const pressed = [];
+const codeWord = "dennis";
+
 window.addEventListener('keyup', (e) => {
     console.log(event.key);
+    pressed.push(e.key);
+    pressed.splice(-codeWord.length - 1, pressed.length - codeWord.length);
+
+    if (pressed.join('').includes(codeWord)){
+        alert("Ding Ding");
+    }
+
+    console.log(pressed);
 })
